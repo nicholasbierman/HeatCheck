668a1b8382e521bad851eb3a3a110ae351fa7aeb
Nick Bierman <biermannick83@gmail.com>
2021-02-04 19:33:57 -0500

    add inner paint lines

--- react-app/src/components/Court/court.js
+++ react-app/src/components/Court/court.js
@@ -8,6 +8,9 @@ export const Court = () => {
             <g class="court">
                 <polyline fill="none" stroke="#999999" strokeLinecap="square" strokeLinejoin="bevel" strokeMiterlimit="10" viewBox="0 0 440 140" points="470,140 470,0 30,0 30,147">
                 </polyline>
+                <rect width="160" height="190" x="170"></rect>
+                <line x1="310" y1="190" x2="310" y2="0"></line>
+                <line x1="190" y1="190" x2="190" y2="0"></line>
             </g>
         </svg>
     )
