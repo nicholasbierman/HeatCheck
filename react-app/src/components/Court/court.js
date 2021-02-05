5da6ec819ddc0f757ff828f470811a76d45dd374
Nick Bierman <biermannick83@gmail.com>
2021-02-04 19:06:59 -0500

    correct viewBox sizing for svg element

--- react-app/src/components/Court/court.js
+++ react-app/src/components/Court/court.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 export const Court = () => {
     return (
-        <svg stroke="orange" fill="purple" viewBox="0 0 711 497.7">
+        <svg stroke="orange" fill="purple" viewBox="0 0 500 470">
             <g fill="white" stroke="green" strokeWidth="5">
-                <circle cx="40" cy="40" r="25">
-                    
-                </circle>
-
             </g>
-            <polyline color="#ffa500" stroke="#ccc" strokeWidth="1">
-
-            </polyline>
+            <g class="court">
+                <polyline fill="none" stroke="#999999" strokeLinecap="square" strokeLinejoin="bevel" strokeMiterlimit="10" viewBox="0 0 440 140" points="470.364,137.064 470.364,0 30.059,0 30.059,137.064">
+                </polyline>
+            </g>
         </svg>
     )
 }
