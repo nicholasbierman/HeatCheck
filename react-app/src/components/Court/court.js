dca2fcd930c4195820d068e8eda08194fe4de10f
Nick Bierman <biermannick83@gmail.com>
2021-02-04 19:08:20 -0500

    correct 'points' attr for short corner 3 and baseline polyline element

--- react-app/src/components/Court/court.js
+++ react-app/src/components/Court/court.js
@@ -6,7 +6,7 @@ export const Court = () => {
             <g fill="white" stroke="green" strokeWidth="5">
             </g>
             <g class="court">
-                <polyline fill="none" stroke="#999999" strokeLinecap="square" strokeLinejoin="bevel" strokeMiterlimit="10" viewBox="0 0 440 140" points="470.364,137.064 470.364,0 30.059,0 30.059,137.064">
+                <polyline fill="none" stroke="#999999" strokeLinecap="square" strokeLinejoin="bevel" strokeMiterlimit="10" viewBox="0 0 440 140" points="470,140 470,0 30,0 30,147">
                 </polyline>
             </g>
         </svg>
