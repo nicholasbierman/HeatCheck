d453fa85fb43d9bbeda79b11dba55207c44129b5
Nick Bierman <biermannick83@gmail.com>
2021-02-04 19:52:36 -0500

    add path element for top of key

--- react-app/src/components/Court/court.js
+++ react-app/src/components/Court/court.js
@@ -11,6 +11,7 @@ export const Court = () => {
                 <rect width="160" height="190" x="170"></rect>
                 <line x1="310" y1="190" x2="310" y2="0"></line>
                 <line x1="190" y1="190" x2="190" y2="0"></line>
+                <path d="M190,190 c0,33,27,60,60,60c33,0,60-27,60-60"></path>
             </g>
         </svg>
     )
