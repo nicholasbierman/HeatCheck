7ba5a0c7caf62d23facb2686e430e714b216483d
Nick Bierman <biermannick83@gmail.com>
2021-02-05 10:20:04 -0500

    add second free throw lane hash on left side

--- react-app/src/components/Court/court.js
+++ react-app/src/components/Court/court.js
@@ -13,11 +13,12 @@ export const Court = () => {
                 <path class="restricted-area" d="M211,53 c3,22,23,37,44,34c18-2,32-16,34-34"></path>
                 <path class="key-bottom" d="M190,190 c0,33,27,60,60,60c33,0,60-27,60-60"></path>
                 <path class="key-top" fill="none" stroke-dasharray="12.92 10.24" d="M310,190c0-33-27-60-60-60c-33,0-60,27-60,60"></path>
-                <line class="right-lane-hash" x1="335" y1="70" x2="330" y2="70"></line> /* 7 feet from baseline, 2 inches wide */
+                <line class="right-lane-hash" x1="335" y1="70" x2="330" y2="70"></line> /* 7 feet from baseline, 6 inches wide */
                 <line class="right-lane-hash"x1="335" y1="80" x2="330" y2="80"></line> /* 8 feet from baseline */
                 <line class="right-lane-hash"x1="335" y1="110" x2="330" y2="110"></line> /* 11 feet from baseline */
                 <line class="right-lane-hash"x1="335" y1="140" x2="330" y2="140"></line> /* 14 feet from baseline */
-                <line class="right-lane-hash" x1="165" y1="70" x2="170" y2="70"></line> /* 7 feet from baseline, 2 inches wide */
+                <line class="left-lane-hash" x1="165" y1="70" x2="170" y2="70"></line> /* 7 feet from baseline, 6 inches wide */
+                <line class="left-lane-hash" x1="165" y1="80" x2="170" y2="80"></line> /* 8 feet from baseline, 6 inches wide */
             </g>
         </svg>
     )
