2048eca6aee71b9af2045d67fa8ac6b87416d163
Nick Bierman <biermannick83@gmail.com>
2021-02-09 11:48:50 -0500

    test request working

--- app/api/player_shot_routes.py
+++ app/api/player_shot_routes.py
@@ -1,19 +1,24 @@
 from flask import Blueprint, request
-from app.models import Shot
 import json
 import requests
 
-base_request_url = "https://stats.nba.com/stats/shotchartdetail?AheadBehind=&CFID=33&CFPARAMS=2020-21&ClutchTime=&Conference=&ContextFilter=&ContextMeasure=FGA&DateFrom=&DateTo=&Division=&EndPeriod=10&EndRange=28800&GROUP_ID=&GameEventID=&GameID=&GameSegment=&GroupID=&GroupMode=&GroupQuantity=5&LastNGames=0&LeagueID=00&Location=&Month=0&OnOff=&OpponentTeamID=0&Outcome=&PORound=0&Period=0&PlayerID=201939&PlayerID1=&PlayerID2=&PlayerID3=&PlayerID4=&PlayerID5=&PlayerPosition=&PointDiff=&Position=&RangeType=0&RookieYear=&Season=2020-21&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StartPeriod=1&StartRange=0&StarterBench=&TeamID=0&VsConference=&VsDivision=&VsPlayerID1=&VsPlayerID2=&VsPlayerID3=&VsPlayerID4=&VsPlayerID5=&VsTeamID="
-
 shot_routes = Blueprint("shots", __name__)
 
-teams = json.loads(requests.get(
-    'https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json')
-    .text)
+base_request_url = "https://stats.nba.com/stats/shotchartdetail?AheadBehind=&CFID=33&CFPARAMS=2020-21&ClutchTime=&Conference=&ContextFilter=&ContextMeasure=FGA&DateFrom=&DateTo=&Division=&EndPeriod=10&EndRange=28800&GROUP_ID=&GameEventID=&GameID=&GameSegment=&GroupID=&GroupMode=&GroupQuantity=5&LastNGames=0&LeagueID=00&Location=&Month=0&OnOff=&OpponentTeamID=0&Outcome=&PORound=0&Period=0&PlayerID=201939&PlayerID1=&PlayerID2=&PlayerID3=&PlayerID4=&PlayerID5=&PlayerPosition=&PointDiff=&Position=&RangeType=0&RookieYear=&Season=2020-21&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StartPeriod=1&StartRange=0&StarterBench=&TeamID=0&VsConference=&VsDivision=&VsPlayerID1=&VsPlayerID2=&VsPlayerID3=&VsPlayerID4=&VsPlayerID5=&VsTeamID="
 
-players = json.loads(requests.get(
-    'https://raw.githubusercontent.com/bttmly/nba/master/data/players.json')
-    .text)
+
+headers_dict = {'origin': 'https://www.nba.com',
+                'referer': 'https://www.nba.com/',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-node': 'cors',
+                'sec-fetch-site': 'same-site',
+                'x-nba-stats-origin': 'stats',
+                'x-nba-stats-token': 'true',
+                'Content-Type': 'application/json',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
+
+r = requests.get(base_request_url, headers=headers_dict)
+print(r.json())
 
 
 def get_player_id(first, last):
@@ -34,8 +39,6 @@ def get_team_id(team):
 def get_shots_by_player_id(player_id):
     shots = Shot.query.all()
     return {"shots": [shot.to_dict() for shot in shots]}
-
-
 
     # print("HITTING BACKEND ROUTE")
     # shot_json = shotchartdetail.ShotChartDetail(
--- app/seeds/shots.py
+++ app/seeds/shots.py
@@ -4,8 +4,18 @@ from app.models import db, Shot
 def seed_shots():
     shot1 = Shot(nba_player_id=201939, x=100, y=100, shot_zone="Center")
     shot2 = Shot(nba_player_id=201939, x=250, y=150, shot_zone="Left Corner 3")
+    shot3 = Shot(nba_player_id=201939, x=250, y=250)
+    shot4 = Shot(nba_player_id=201939, x=0, y=0)
+    shot5 = Shot(nba_player_id=201939, x=500, y=0)
+    shot6 = Shot(nba_player_id=201939, x=0, y=470)
+    shot7 = Shot(nba_player_id=201939, x=0, y=460)
     db.session.add(shot1)
     db.session.add(shot2)
+    db.session.add(shot3)
+    db.session.add(shot4)
+    db.session.add(shot5)
+    db.session.add(shot6)
+    db.session.add(shot7)
     db.session.commit()
 
 
