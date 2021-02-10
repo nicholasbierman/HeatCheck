84439f4b70a9e849cc9b555f32167a0e784eb036
Nick Bierman <biermannick83@gmail.com>
2021-02-09 21:09:28 -0500

    add thunk for inserting leagueAverages into state

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import shots from './shots'
+import leagueAverages from './leagueAverages'
 
 const rootReducer = combineReducers({
-  shots
+  shots,
+  leagueAverages
 });
 
 let enhancer;
--- react-app/src/store/leagueAverages.js
+++ react-app/src/store/leagueAverages.js
@@ -6,5 +6,22 @@ const setLeagueAverages = (leagueAverages) => ({
 })
 
 export const getLeagueAverages = () => async (dispatch) => {
-    const response = await fetch()
+    const response = await fetch('/api/league_averages/')
+    const data = await response.json()
+    dispatch(setLeagueAverages(data.league_averages))
 }
+
+const initialState = { leagueAverages: [] }
+
+function reducer (state = initialState, action) {
+    let newState;
+    switch (action.type) {
+        case SET_LEAGUE_AVERAGES:
+            newState = { ...state, leagueAverages: action.payload }
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
