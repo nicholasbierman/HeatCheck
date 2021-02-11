9f7952f6ce98151997b6d503f5a4d67cd55837d7
Nick Bierman <biermannick83@gmail.com>
2021-02-11 12:42:19 -0500

    add player efficiency legends

--- react-app/src/components/Court/index.js
+++ react-app/src/components/Court/index.js
@@ -35,11 +35,17 @@ export const Court = () => {
                 <line className="left-lane-hash" x1="165" y1="110" x2="170" y2="110"></line> {/* 11 feet from baseline, 6 inches wide */}
                 <line className="left-lane-hash" x1="165" y1="140" x2="170" y2="140"></line> {/* 14 feet from baseline, 6 inches wide */}
                 {/* 3 point line has parallel lines 3' from sidelines and an arc of 23'4" from the middle of the basket */}
-                <path fill="none" className="three-point-line-center" d="M30,140 c50,122,190,180,310,130c60-24.9,106-71,130-130"></path>
+                    <path fill="none" className="three-point-line-center" d="M30,140 c50,122,190,180,310,130c60-24.9,106-71,130-130"></path>
             </g>
             <g className="hoop">
                 <line className="backboard" x1="280" y1="40" x2="220" y2="40"></line> {/* 4 feet from baseline, measures 6' horizontally */}
-                <path stroke="orange" className="rim" d="M250,55 c4,0,7-3,7-7c0-4-4-7-7-7c-4,0-7.197,3-7,7 C243,52,246,55,250,55z"></path>
+                    <path stroke="orange" className="rim" d="M250,55 c4,0,7-3,7-7c0-4-4-7-7-7c-4,0-7.197,3-7,7 C243,52,246,55,250,55z"></path>
+                </g>
+                <g className="efficiency-legend">
+                    <rect x="350" y="450" width="15" height="15" rx="2" ry="2" fill="blue"></rect>
+                    <text x="250" y="465">Less Efficient</text>
+                    <rect x="380" y="450" width="15" height="15" rx="2" ry="2" fill="red"></rect>
+                    <text x="400" y="465">More Efficient</text>
             </g>
             <g>
                     { shots && shots.map(shot => {
@@ -47,7 +53,7 @@ export const Court = () => {
                             <ShotMark x={ shot.x } y={ shot.y }/>
                     )
                 })}
-            </g>
+                </g>
             </svg>
         </div>
 
