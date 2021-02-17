9c8b377687c78a03e103a26f7c5b55d58152e5f3
Nick Bierman <biermannick83@gmail.com>
2021-02-17 16:11:30 -0500

    add first navlink component and main title

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <nav style={ { height: "fit-content", color: "whitesmoke", margin: "0 0 60px 0", display: "flex", backgroundColor: "orange", width: "100%" } }>
-      <ul style={ { listStyle: "none", display: "flex" } }>
-        <li>HeatCheck</li>
-        <li>Player Shot Charts</li>
+      <ul style={ { listStyle: "none", display: "flex", alignItems: "center", width: "100%", justifyContent: "start" } }>
+        <li style={{marginRight: "10%", fontSize: "30px"}}>HeatCheck🔥✅</li>
+        <NavLink to="/" style={{textDecoration: "none", color: "whitesmoke"}}><li>Player Shot Charts</li></NavLink>
       </ul>
     </nav>
   );
