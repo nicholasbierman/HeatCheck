0f5851ada22effe9252e87af1565121c1cc56b8d
Nick Bierman <biermannick83@gmail.com>
2021-02-17 16:29:11 -0500

    update app title in index.html

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ export const NavBar = () => {
     <nav style={ { height: "fit-content", color: "whitesmoke", margin: "0 0 60px 0", display: "flex", backgroundColor: "orange", width: "100%" } }>
       <ul style={ { listStyle: "none", display: "flex", alignItems: "center", width: "100%", justifyContent: "start" } }>
         <li style={{marginRight: "10%", fontSize: "30px"}}>HeatCheck🔥✅</li>
-        <NavLink to="/" style={{textDecoration: "none", color: "whitesmoke"}}><li>Player Shot Charts</li></NavLink>
+        <NavLink to="/" style={{textDecoration: "none", color: "whitesmoke"}}><li>Shot Charts</li></NavLink>
       </ul>
     </nav>
   );
