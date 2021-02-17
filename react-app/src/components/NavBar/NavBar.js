51d91798bcf78d31148b29f96a7a31741ed6dd50
Nick Bierman <biermannick83@gmail.com>
2021-02-17 15:55:56 -0500

    initial navbar setup

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -3,8 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <nav style={ { height: "15px", color: "orange", margin: "0 auto" } }>
-      <ul>
+    <nav style={ { height: "fit-content", color: "whitesmoke", margin: "0 0 60px 0", display: "flex", backgroundColor: "orange", width: "100%" } }>
+      <ul style={ { listStyle: "none", display: "flex" } }>
+        <li>HeatCheck</li>
+        <li>Player Shot Charts</li>
       </ul>
     </nav>
   );
