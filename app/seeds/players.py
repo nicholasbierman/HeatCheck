1ef6be6faeadd87ac65071851f32cdf48b534f4d
Nick Bierman <biermannick83@gmail.com>
2021-02-18 08:30:20 -0500

    fix issue where bruno caboclo was not inserting into psql properly

--- app/seeds/players.py
+++ app/seeds/players.py
@@ -8,17 +8,25 @@ def seed_players():
         'https://raw.githubusercontent.com/bttmly/nba/master/data/players.json').text)
     for player in players:
         if player['teamId'] == 0:
-            pass
-        new_player = Player(
-            first_name=player["firstName"],
-            last_name=player["lastName"],
-            team_id=player["teamId"],
-            nba_player_id=player["playerId"]
+            caboclo = Player(
+                first_name=player['firstName'],
+                last_name=player["lastName"],
+                team_id=1610612745,
+                nba_player_id=player["playerId"]
             )
-        
-        db.session.add(new_player)
-        db.session.commit()
-        db.session.flush()
+            db.session.add(caboclo)
+            db.session.commit()
+            db.session.flush()
+        else:
+            new_player = Player(
+                first_name=player["firstName"],
+                last_name=player["lastName"],
+                team_id=player["teamId"],
+                nba_player_id=player["playerId"]
+            )
+            db.session.add(new_player)
+            db.session.commit()
+            db.session.flush()
 
 
 def undo_players():
