3d932b13371f7b56cb3b95d8afe15c3da4a1d425
Nick Bierman <biermannick83@gmail.com>
2021-02-19 17:24:26 -0500

    THIS VERSION WORKS FOR FETCHING DATA FROM STATS.NBA.COM

--- react-app/src/components/Zone/Zone.js
+++ react-app/src/components/Zone/Zone.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Zone = () => {
     return (
-        <g onMouseEnter>
+        <g>
             <path style={{fillOpacity: "0.3"}} fill="purple" className="zone right-mid-range" d="M470,140, 470,0, 330,0 330,140"></path>
         </g>
     )
