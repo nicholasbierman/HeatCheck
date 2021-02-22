9644920b576a9eb56a640870aefa4c43cab812fb
Nick Bierman <biermannick83@gmail.com>
2021-02-22 12:13:06 -0500

    fix dispatch event on PlayerSelector component

--- react-app/src/components/PlayerSelector/PlayerSelector.js
+++ react-app/src/components/PlayerSelector/PlayerSelector.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAllPlayers } from '../../store/allPlayers';
 
 export const PlayerSelector = () => {
-    const players = useSelector(state => state.players);
+    const players = useSelector(state => state.allPlayers.allPlayers);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getAllPlayers())
