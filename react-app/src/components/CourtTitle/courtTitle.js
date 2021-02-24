c1405faaee6e1893e089abcbc3590b309181f474
Nick Bierman <biermannick83@gmail.com>
2021-02-24 15:22:36 -0500

    update style obj for CourtTitle

--- react-app/src/components/CourtTitle/courtTitle.js
+++ react-app/src/components/CourtTitle/courtTitle.js
@@ -7,8 +7,8 @@ export const CourtTitle = () => {
     const shots = useSelector(state => state.shots.shots);
 
     return (
-        <>
+        <div style={{width: "75%", margin: "1% 25% 0 25%"}}>
             <h1 style={ { color: "whitesmoke" } }>{ player.first_name } {player.last_name } | 2020-2021 Regular Season | {shots.length } Shots</h1>
-        </>
+        </div>
     )
 }
