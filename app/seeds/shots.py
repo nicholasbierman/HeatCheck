eef698f0ebb2e676448b32c09bad990d6ac295c6
Nick Bierman <biermannick83@gmail.com>
2021-02-25 11:25:50 -0500

    seed zion data

--- app/seeds/shots.py
+++ app/seeds/shots.py
@@ -8,16 +8,26 @@ def seed_shots():
         data = json.load(file)
         shot_list = data["resultSets"][0]["rowSet"]
         for shot in shot_list:
-            new_shot = Shot(nba_player_id=shot[3], 
-            x=shot[17]+250, 
-            y=shot[18]+60,
-            shot_zone=f'{shot[13]} - {shot[14]}', 
-            shot_made_flag=shot[20])
+            new_shot = Shot(nba_player_id=shot[3],
+                            x=shot[17]+250,
+                            y=shot[18]+60,
+                            shot_zone=f'{shot[13]} - {shot[14]}',
+                            shot_made_flag=shot[20])
             db.session.add(new_shot)
             db.session.commit()
             db.session.flush()
 
     with open('/Users/nick/Desktop/app-academy/projects/heat-check/app/seeds/harden_shot_data.txt') as file:
+        data = json.load(file)
+        shot_list = data["resultSets"][0]["rowSet"]
+        for shot in shot_list:
+            new_shot = Shot(nba_player_id=shot[3], x=shot[17]+250, y=shot[18]+60,
+                            shot_zone=f'{shot[13]} - {shot[14]}', shot_made_flag=shot[20])
+            db.session.add(new_shot)
+            db.session.commit()
+            db.session.flush()
+
+    with open('/Users/nick/Desktop/app-academy/projects/heat-check/app/seeds/zion_shot_data.txt') as file:
         data = json.load(file)
         shot_list = data["resultSets"][0]["rowSet"]
         for shot in shot_list:
