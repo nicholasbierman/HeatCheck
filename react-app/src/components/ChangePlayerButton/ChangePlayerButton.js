fab7a4a7c958e0e8d8d975ccd4bd47b0d7e2fa68
Nick Bierman <biermannick83@gmail.com>
2021-02-25 11:19:35 -0500

    remove PlayerSelector component

--- react-app/src/components/ChangePlayerButton/ChangePlayerButton.js
+++ react-app/src/components/ChangePlayerButton/ChangePlayerButton.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { getPlayerById } from '../../store/player';
 import { getShotsByPlayerId } from '../../store/shots';
+import { getAllPlayers } from '../../store/allPlayers';
+
 
 export const ChangePlayerButton = () => {
     const dispatch = useDispatch();
@@ -11,13 +13,17 @@ export const ChangePlayerButton = () => {
         dispatch(getPlayerById(selectedPlayer))
         dispatch(getShotsByPlayerId(selectedPlayer));
     }, [ selectedPlayer ])
+
+    useEffect(() => {
+        dispatch(getAllPlayers())
+    }, [dispatch])
     
     
     return (
         <form onSubmit={(e) => e.preventDefault()}>
             <select style={ {cursor: "pointer", backgroundColor: "blueviolet", borderColor: "transparent", cursor: "pointer", borderRadius: "4px"} } onChange={(e) => setSelectedPlayer(e.target.value)}>
                 {allPlayers.map((player, i) => {
-                    return <option key={ i } value={player.nba_player_id}>{ player.first_name } { player.last_name }</option>
+                    return <option key={ i } value={player.nba_player_id}>{ player.last_name }, { player.first_name }</option>
                 })}
             </select>
         {/* <button style={{backgroundColor: "blueviolet", borderColor: "transparent", cursor: "pointer", borderRadius: "4px", width: "10%"}}>Change Player
