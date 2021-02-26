9bf9dd17ca8bf344b7986c02a3971f0d76f0b56c
Nick Bierman <biermannick83@gmail.com>
2021-02-25 23:06:30 -0500

    backend console error cleanup

--- app/seeds/shots.py
+++ app/seeds/shots.py
@@ -21,8 +21,10 @@ def seed_shots():
         data = json.load(file)
         shot_list = data["resultSets"][0]["rowSet"]
         for shot in shot_list:
-            new_shot = Shot(nba_player_id=shot[3], x=shot[17]+250, y=shot[18]+60,
-                            shot_zone=f'{shot[13]} - {shot[14]}', shot_made_flag=shot[20])
+            new_shot = Shot(nba_player_id=shot[3], x=shot[17]+250,
+                            y=shot[18]+60,
+                            shot_zone=f'{shot[13]} - {shot[14]}',
+                            shot_made_flag=shot[20])
             db.session.add(new_shot)
             db.session.commit()
             db.session.flush()
@@ -31,8 +33,10 @@ def seed_shots():
         data = json.load(file)
         shot_list = data["resultSets"][0]["rowSet"]
         for shot in shot_list:
-            new_shot = Shot(nba_player_id=shot[3], x=shot[17]+250, y=shot[18]+60,
-                            shot_zone=f'{shot[13]} - {shot[14]}', shot_made_flag=shot[20])
+            new_shot = Shot(nba_player_id=shot[3], x=shot[17]+250,
+                            y=shot[18]+60,
+                            shot_zone=f'{shot[13]} - {shot[14]}',
+                            shot_made_flag=shot[20])
             db.session.add(new_shot)
             db.session.commit()
             db.session.flush()
