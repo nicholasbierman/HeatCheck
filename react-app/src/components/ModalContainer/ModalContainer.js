78d229e975fe0efe85da577261853af8839cc208
Nick Bierman <biermannick83@gmail.com>
2021-03-03 11:42:37 -0500

    update ModalContainer to use contact-card devicon

--- react-app/src/components/ModalContainer/ModalContainer.js
+++ react-app/src/components/ModalContainer/ModalContainer.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { Modal } from '../Modal/Modal';
-
+import React, { useState } from "react";
+import { Modal } from "../Modal/Modal";
 
 export const ModalContainer = () => {
-    const [ showModal, setShowModal ] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const showModalOnHover = (e) => {
-        e.stopPropagation()
-        setShowModal(true)
-    }
+  const showModalOnHover = (e) => {
+    e.stopPropagation();
+    setShowModal(true);
+  };
 
-    const hideModalOnHover = (e) => {
-        e.stopPropagation()
-        setShowModal(false);
-    }
-    return (
-        <div style={{maxWidth: "100%"}}>
-            <button onMouseOver={(e) => showModalOnHover(e)}>About Me</button>
-            {showModal ? <Modal /> : null}
-        </div>
-
-    )
-}
+  const hideModalOnHover = (e) => {
+    e.stopPropagation();
+    setShowModal(false);
+  };
+  return (
+    <div style={{ maxWidth: "100%" }}>
+      <button onMouseOver={(e) => showModalOnHover(e)}>
+        <i style={{ fontSize: "20px" }} class="fas fa-id-card"></i>
+      </button>
+      {showModal ? <Modal /> : null}
+    </div>
+  );
+};
