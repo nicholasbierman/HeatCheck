a821eaf569580c7bd7c6d63bfc1096637077bc45
Nick Bierman <biermannick83@gmail.com>
2021-03-11 10:31:45 -0500

    update old <Modal> to <InfoCard>

--- react-app/src/components/ModalContainer/ModalContainer.js
+++ react-app/src/components/ModalContainer/ModalContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal } from "../Modal/Modal";
+import { Modal } from "../InfoCard/InfoCard";
 
 export const ModalContainer = () => {
   const [showModal, setShowModal] = useState(false);
