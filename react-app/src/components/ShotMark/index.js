a2fbccbd0ba84a11ba12b8b09ac41565ed4bada8
Nick Bierman <biermannick83@gmail.com>
2021-04-15 10:49:44 -0400

    export default Zone

--- react-app/src/components/ShotMark/index.js
+++ react-app/src/components/ShotMark/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const ShotMark = ({ x, y, comparison }) => {
     return (
-        <rect rx="1" height="5px" width="5px" fill={comparison ? "#FE00FB" : "lime"} x={ x } y={ y }>
+        <rect rx="1" height="5px" width="5px" fill={comparison ? "#FE00FB" : "lime"} x={x} y={y}>
 
-            </rect>
-    )
-}
+        </rect>
+    );
+};
 
 export default ShotMark;
