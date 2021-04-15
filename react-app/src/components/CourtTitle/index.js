56bfaf90a68b138092bc15ee59c34b58181f0156
Nick Bierman <biermannick83@gmail.com>
2021-04-15 10:46:37 -0400

    export default CourtTitle

--- react-app/src/components/CourtTitle/index.js
+++ react-app/src/components/CourtTitle/index.js
@@ -10,7 +10,9 @@ export const CourtTitle = () => {
     return (
         <div style={{ width: "75%", margin: "1% 25% 0 25%" }}>
             <ChangePlayerButton />
-            <h1 style={ { color: "whitesmoke" } }>{ player.first_name } {player.last_name } | 2020-2021 Regular Season | {shots.length } Shots</h1>
+            <h1 style={{ color: "whitesmoke" }}>{player.first_name} {player.last_name} | 2020-2021 Regular Season | {shots.length} Shots</h1>
         </div>
-    )
-}
+    );
+};
+
+export default CourtTitle;
