8cd70a04a3d0d01d3efe74434ffc95964565048c
Nick Bierman <biermannick83@gmail.com>
2021-04-15 10:39:14 -0400

    update file paths to use 'index' instead of component name

--- react-app/src/components/ChangePlayerButton/index.js
+++ react-app/src/components/ChangePlayerButton/index.js
@@ -7,7 +7,7 @@ import { getComparisonPlayerById } from '../../store/comparisonPlayer';
 import { getComparisonShotsByPlayerId } from '../../store/comparisonShots';
 
 
-export const ChangePlayerButton = ({comparison}) => {
+export const ChangePlayerButton = ({ comparison }) => {
     const dispatch = useDispatch();
     const allPlayers = useSelector(state => state.allPlayers.allPlayers);
     const [ selectedPlayer, setSelectedPlayer ] = useState(null);
@@ -16,27 +16,28 @@ export const ChangePlayerButton = ({comparison}) => {
     useEffect(() => {
         dispatch(getPlayerById(selectedPlayer));
         dispatch(getShotsByPlayerId(selectedPlayer));
-    }, [dispatch, selectedPlayer]);
+    }, [ dispatch, selectedPlayer ]);
 
     useEffect(() => {
         dispatch(getComparisonPlayerById(comparisonPlayer));
         dispatch(getComparisonShotsByPlayerId(comparisonPlayer));
-    }, [dispatch, comparisonPlayer])
+    }, [ dispatch, comparisonPlayer ]);
 
     useEffect(() => {
-        dispatch(getAllPlayers())
-    }, [dispatch])
+        dispatch(getAllPlayers());
+    }, [ dispatch ]);
     
     return (
         <form onSubmit={(e) => e.preventDefault()}>
             <select style={{ backgroundColor: "slategray", borderColor: "transparent", cursor: "pointer", borderRadius: "4px" }} onChange={(e) => comparison ? setComparisonPlayer(e.target.value) : setSelectedPlayer(e.target.value)}>
                 {comparison ? <option defaultValue={selectedPlayer} hidden>Compare to...</option> : <option defaultValue={comparisonPlayer} hidden>Change Player...</option>}
                 {allPlayers.map((player, i) => {
-                    return <option key={ i } value={player.nba_player_id}>{ player.last_name }, { player.first_name }</option>
+                    return <option key={i} value={player.nba_player_id}>{player.last_name}, {player.first_name}</option>;
                 })}
             </select>
-        {/* <button style={{backgroundColor: "blueviolet", borderColor: "transparent", cursor: "pointer", borderRadius: "4px", width: "10%"}}>Change Player
+            {/* <button style={{backgroundColor: "blueviolet", borderColor: "transparent", cursor: "pointer", borderRadius: "4px", width: "10%"}}>Change Player
         </button> */}
         </form>
-    )
-}
+    );
+};
+
